ba2511fd703d94693d8bb3fc888c5cc8f616f04d
Giorgio Sironi <info@giorgiosironi.com>
2016-02-07 09:59:19 +0100

    Generating a range

--- client/app/planes/planes.controller.js
+++ client/app/planes/planes.controller.js
@@ -2,6 +2,25 @@
 
 angular.module('gameOfLifeJavascriptApp')
   .controller('PlanesCtrl', function ($scope, $routeParams) {
+    var range = function(start, length) {
+      var result = [];
+      for (var i = start; i < start + length; i++) {
+        result.push(i);
+      }
+      return result;
+    };
+
     $scope.message = 'Hello';
     $scope.name = $routeParams.name;
+    $scope.plane = {};
+    $scope.plane.size = {};
+    $scope.plane.size.x = {};
+    $scope.plane.size.x.range = range(0, 10);
+    $scope.plane.size.y = {};
+    $scope.plane.size.y.range = range(0, 10);
+    $scope.plane.aliveCells = [
+      { x: 2, y: 2 },
+      { x: 2, y: 3 },
+      { x: 2, y: 4 }
+    ];
   });
