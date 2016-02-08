2178b8a99a7c4127395e5d58c6b43a2d414496d1
Giorgio Sironi <info@giorgiosironi.com>
2016-02-08 16:49:59 +0100

    Obsolete responsibilities of the MainController

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,25 +4,6 @@
 
 class MainController {
 
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
 }
 
 angular.module('gameOfLifeJavascriptApp')
