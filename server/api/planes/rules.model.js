44ff133aa4696baeb12c6c16b0a14ec0de291627
Giorgio Sironi <info@giorgiosironi.com>
2016-02-08 16:46:18 +0100

    jshint checks are satisfied again

--- server/api/planes/rules.model.js
+++ server/api/planes/rules.model.js
@@ -2,7 +2,7 @@
 
 var Rules = {
   nextState: function(currentState, aliveNeighbors) {
-    if (aliveNeighbors == 3) {
+    if (aliveNeighbors === 3) {
       return true;
     }
     if (aliveNeighbors >= 2 && aliveNeighbors <= 3) {
