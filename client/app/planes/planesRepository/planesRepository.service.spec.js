7bf1fb9ad496c632a0ab64f9113a76c5e780cccd
Giorgio Sironi <info@giorgiosironi.com>
2016-02-09 15:11:31 +0100

    Unit tests for planesRepository

--- client/app/planes/planesRepository/planesRepository.service.spec.js
+++ client/app/planes/planesRepository/planesRepository.service.spec.js
@@ -2,17 +2,26 @@
 
 describe('Service: planesRepository', function () {
 
-  // load the service's module
   beforeEach(module('gameOfLifeJavascriptApp'));
 
-  // instantiate service
-  var planesRepository;
-  beforeEach(inject(function (_planesRepository_) {
+  var planesRepository, $httpBackend;
+
+  beforeEach(inject(function (_planesRepository_, _$httpBackend_) {
     planesRepository = _planesRepository_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  it('should do something', function () {
-    expect(!!planesRepository).toBe(true);
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
 
+  it('should load the generation alive cells', function () {
+    var spy = jasmine.createSpy();
+    $httpBackend.expect('GET', '/api/planes/a-block-and-bar/generation/3').respond({
+      aliveCells: [{x: 1, y: 2}]
+    });
+    planesRepository.findByName('a-block-and-bar', 3).then(spy);
+    $httpBackend.flush();
+  });
 });
