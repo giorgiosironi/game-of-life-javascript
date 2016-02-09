161f1816760aae0223618f9c0287b1e7b56c331f
Giorgio Sironi <info@giorgiosironi.com>
2016-02-09 11:21:48 +0100

    Added cell directive with tooltip and hover

---
+++ client/app/planes/cell/cell.directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('gameOfLifeJavascriptApp')
+  .directive('cell', function () {
+    return {
+      restrict: 'A',
+      template: '<a title="x={{ cellX }}, y={{ cellY }}" class="cell_tooltip"></a>',
+      scope: {
+        cellX: '@cellX',
+        cellY: '@cellY',
+      }
+    };
+  });
---
+++ client/app/planes/cell/cell.directive.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Directive: cell', function () {
+
+  // load the directive's module
+  beforeEach(module('gameOfLifeJavascriptApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<td cell cell-x="1" cell-y="2"></td>');
+    element = $compile(element)(scope)[0];
+    // TODO: extract the correct element without [0]
+    var a = element.querySelectorAll('a.cell_tooltip')[0];
+    // TODO: assert it's an <a>
+    // TODO: assert on attributes title and class
+    // TODO: assert there is no text
+    expect(a.outerHTML).toBe('<a title="x={{ cellX }}, y={{ cellY }}" class="cell_tooltip"></a>');
+  }));
+});
